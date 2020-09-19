8c56b776577c0cfbd31355be1db7032d992b9a09
Andi_malia010710 <011007andimalia@gmail.com>
2020-09-19 11:17:10 +0700

    Tugas

---
+++ AngryBird/Assets/Script/BombBird.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombBird : Bird
+{
+    public GameObject explosion;
+
+    public void OnCollisionEnter2D(Collision2D wood)
+    {
+        if (wood.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Hit");
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            Destroy(wood.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
